82d432e794431c28d4d8ae263f4fd1a60080f32d
MagicJJ34 <milosz.makiel3@gmail.com>
2025-06-30 22:21:06 +0200

    Przygotowanie pliku do zbioru zadan

---
+++ Projekt/Zadanie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    public class Zadanie
+    {
+        public string Opis { get; set; }
+        public bool Wykonane { get; set; }
+
+    }
+}
