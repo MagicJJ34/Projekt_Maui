46c7a086086a33f7618c2c3563d6510c40609ae4
MagicJJ34 <milosz.makiel3@gmail.com>
2025-06-30 22:19:45 +0200

    Przygotowanie pliku pod ListaZadan.xaml

---
+++ Projekt/ListaZadan.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    public partial class ListaZadan : ContentPage
+    {
+        public ObservableCollection<Zadanie> Zadania { get; set; } = new ObservableCollection<Zadanie>();
+        public ListaZadan()
+
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+        private void DodajZadanie_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(PoleZadania.Text))
+            {
+                Zadania.Add(new Zadanie { Opis = PoleZadania.Text, Wykonane = false });
+                PoleZadania.Text = string.Empty;
+            }
+        }
+        private void UsunZadanie_Clicked(object sender, EventArgs e)
+        {
+            var przycisk = sender as Button;
+            var zadanie = przycisk?.CommandParameter as Zadanie;
+            if (zadanie != null)
+            {
+                Zadania.Remove(zadanie);
+            }
+        }
+    }
+}
+
