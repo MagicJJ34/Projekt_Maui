4ef509bb63ff68c62fc15ce8ccbd56bd98d8e7f8
MagicJJ34 <milosz.makiel3@gmail.com>
2025-06-30 22:23:30 +0200

    Przygotowanie pliku pod MainPage.xaml

--- Projekt/MainPage.xaml.cs
+++ Projekt/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace Projekt
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace Projekt
 {
     public partial class MainPage : ContentPage
     {
@@ -6,6 +9,13 @@
         {
             InitializeComponent();
         }
+        private void NowyPlik_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+        private void WczytajPlik_Clicked(object sender, EventArgs e)
+        {
+            
+        }
     }
-
 }
